c2a63e1fc55f349e78d88774238dea456aa87434
jpsanchezm1 <jp.sanchezm1@uniandes.edu.co>
2022-08-24 12:55:32 -0500

    Latest adds

---
+++ Graficas.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 23 20:32:08 2022
+
+@author: ACER
+"""
+
+"    GRAFICAS    "
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def Sucesion_Fibonnaci(x):
+    
+    sf=[0,1]
+    
+    i=0
+    j=1
+    while len(sf)<=x:
+        n=sf[i]+sf[j]
+        sf.append(n)
+        i+=1
+        j+=1
+    R=sf[1:x+1]    
+    return R
+
+
+def Sucesion_Fibonnaci_G(x):
+    
+    sf=[0,1]
+    
+    i=0
+    j=1
+    while len(sf)<=x:
+        n=sf[i]+sf[j]
+        sf.append(n)
+        i+=1
+        j+=1
+        
+    return sf
+
+def Grafica_Sucesion_Fibonnaci(x):
+    R=plt.plot(Sucesion_Fibonnaci_G(x),"r")
+    plt.title("Fibonacci")
+    plt.legend(("Serie Fibonacci",)
+               ,prop={"size":13},loc="upper left")
+        
+    return R
+
+plt.show(Grafica_Sucesion_Fibonnaci(20))
+
+
+
+def Numero_Aureo(x):
+    
+    sf = Sucesion_Fibonnaci(x+1)
+    i=0
+    j=1
+    eje_y=[]
+    while j < x+1:
+        razon= sf[j]/sf[i]
+        eje_y.append(razon)
+        i+=1
+        j+=1
+        
+    return eje_y
+
+    
+
+def Numero_Aureo_G(x):
+    eje_y = Numero_Aureo(x)
+    RAIZ_5 = 5**(1/2)
+    A= (1+RAIZ_5)/2
+    aureo=[A]*19
+    R = plt.plot(eje_y,"b",aureo,"r_")
+    plt.title("Aproximacio al numero aureo")
+    plt.legend(("Estimacion usando la serie","Numero Aureo")
+               ,prop={"size":13},loc="upper right")
+    
+    return R
+
+plt.show(Numero_Aureo_G(19))
+
+
+
+#############################################################
+
+
+def numero_primo(x:int)->bool:
+    R=True
+    for i in range(2,x):
+        
+        if x%i == 0:
+            R=False
+            
+    return R
+
+def primeros_numeros_primos(x)->list:
+
+    R=[]
+    i=2
+    while len(R)<x:
+        c=numero_primo(i)
+        if c == True:       
+        
+            R.append(i)
+            i +=1
+        else:
+            i+=1
+    
+    return R
+
+    
+def grafica_numeros_primos(x):
+    #eje_x= list(range(1,x+1))
+    eje_y= primeros_numeros_primos(x)
+    eje_y[0:0]=[0]
+    #p1=plt.plot(eje_x,eje_y,eje_x,eje_y,"o",)
+    p1=plt.plot(eje_y,"y",eje_y,"or")
+    
+    return p1 
+
+plt.show(grafica_numeros_primos(1000))  
+
+
+
+
